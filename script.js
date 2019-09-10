ec3373d71fd33b6f63fcbeba51ebdaebc47db8c0
Luca Donno <30298476+lucadonnoh@users.noreply.github.com>
2019-09-10 16:37:33 +0200

    correct default color mode and correct switch

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 $(document).ready(function(){
-	
+    
+    if(matchMedia('(prefers-color-scheme: dark)').matches) {
+        $('body').addClass('night-mode');
+    }
+
+    if(matchMedia('(prefers-color-scheme: light)').matches) {
+        $('body').addClass('light-mode');
+    }
+ 
     $("#moon-toggle").click(function() {
-    	$('body').toggleClass('night-mode');
+        $('body').toggleClass('night-mode');
+        $('body').toggleClass('light-mode');
     	return false;
     });
+
+    
 });
